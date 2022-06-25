7b060ffc40453fc43512c41b8d5691d9f49f7aee
Leon San José Larsson <leonlarsson8@gmail.com>
2022-06-25 20:34:36 +0200

    use djs REST for command deploying

--- slash_commands/removeGuildCommands.js
+++ slash_commands/removeGuildCommands.js
@@ -1,12 +1,11 @@
 import { REST } from "@discordjs/rest";
 import { Routes } from "discord-api-types/v10";
-import data from "./data.js";
 import { clientId } from "../config.js";
 import { getToken } from "../src/utils/getToken.js";
 
 const guildId = "99183009621622784";
 const rest = new REST({ version: "10", authPrefix: "Bearer" }).setToken(await getToken());
-await rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: data })
+await rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: [] })
     .then(() => {
-        console.log(`Successfully PUT application commands in guild ${guildId}.`);
+        console.log(`Successfully removed application commands in guild ${guildId}.`);
     }).catch(err => console.log);
